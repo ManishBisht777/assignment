25d79ae1baf244d79f803d7824fbf104033509c2
manish <manishbisht9711@gmail.com>
2022-11-10 22:44:50 +0530

    Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./app.css";
+
+const App = () => (
+  <div className="text-3xl text-black">
+    hello
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, rerum!
+    </p>
+    <img
+      src="https://i.picsum.photos/id/271/536/354.jpg?hmac=y85-qoLKZmshtoXZ3dm9QbbYTwi9kxsQrwut3tmjdJQ"
+      alt=""
+    />
+  </div>
+);
+
+export default App;
